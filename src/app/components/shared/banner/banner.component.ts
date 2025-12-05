585eb8d07647d74e962a4a463cfb90383c33dfa8
tuleshghelani <ghelanitulesh@gmail.com>
2025-12-05 16:50:44 +0530

    refactor(banner): update banner component for improved layout and styling
    - Removed the Company Profile link from the header.
    - Redesigned the banner component to feature a hero section with a wave effect.
    - Enhanced the banner's HTML structure for better accessibility and responsiveness.
    - Updated SCSS styles for the hero banner, including new color schemes and layout adjustments.
    - Introduced a highlight text feature in the banner component for better emphasis on titles.

--- src/app/components/shared/banner/banner.component.ts
+++ src/app/components/shared/banner/banner.component.ts
@@ -5,13 +5,13 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
   @Input() title: string = '';
+  @Input() highlightText?: string;
   @Input() subtitle?: string;
   @Input() breadcrumb: string[] = [];
-  @Input() background?: string = 'assets/images/banner-bg.jpg'; 
 }
